4fe5afcf92328c787f9717176094d367c891a5de
ugaleabhi <ugaleabhishek34@gmail.com>
2025-04-06 22:54:07 +0530

    update file

--- index.js
+++ index.js
@@ -6,21 +6,28 @@ const url=require("url");
 
 const myServer=http.createServer((req,res)=>{
     if(req.url==="/favicon.ico")return res.end();
-    const log=`${Date.now()}:${req.url}New Req Received\n`;
+    const log=`${Date.now()}:${req.method}${req.url}New Req Received\n`;
     const myUrl=url.parse(req.url,true);
     console.log(myUrl);
 
     fs.appendFile('log.txt',log,(err,data)=>{
         switch(myUrl.pathname){
-            case"/":res.end('HomePage');
+            case"/":
+            if(req.method==='GET')res.end('Home Page')
             break;
-            case"/about":
+
+            case "/about":
             const username=myUrl.query.myname
             res.end(`Hi, ${username}`);
             break;
             case"/search":
             const search=myUrl.query.search_query;
             res.end("Here are your result for "+search)
+            case'/signup':
+            if(req.method==='GET')res.end('This is Signup Form');
+            else if(req.method==='Post'){
+                res.end("Success");
+            }
             default :
             res.end("404 Not Found");
         }

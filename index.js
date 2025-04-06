b98f463f8200fed84d156eaa8ab348c127d0bd11
ugaleabhi <ugaleabhishek34@gmail.com>
2025-04-06 19:38:28 +0530

    update file

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const http =require('http');
 const fs=require("fs");
-const { error } = require('console');
+ const { error } = require('console');
+const url=require("url");
+
 
 const myServer=http.createServer((req,res)=>{
+    if(req.url==="/favicon.ico")return res.end();
     const log=`${Date.now()}:${req.url}New Req Received\n`;
+    const myUrl=url.parse(req.url,true);
+    console.log(myUrl);
 
     fs.appendFile('log.txt',log,(err,data)=>{
-        switch(req.url){
+        switch(myUrl.pathname){
             case"/":res.end('HomePage');
             break;
-            case"/about":res.end("I am ABhi");
+            case"/about":
+            const username=myUrl.query.myname
+            res.end(`Hi, ${username}`);
             break;
+            case"/search":
+            const search=myUrl.query.search_query;
+            res.end("Here are your result for "+search)
             default :
             res.end("404 Not Found");
         }
